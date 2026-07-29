e43ed3ddbbbbedf290acefc90a2831429302efae
Stanis-lav95 <Stafik950@yandex.ru>
2026-07-29 00:46:44 +0000

    scroll repair

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,60 @@
-var img = document.querySelector('.aboutMe-img')
+var img = document.querySelector('.aboutMe-img');
 
 function handleMove(x, y, rect) {
-	var rotateX = (y / rect.height - 0.5) * 25
-	var rotateY = (x / rect.width - 0.5) * -25
+    var rotateX = (y / rect.height - 0.5) * 25;
+    var rotateY = (x / rect.width - 0.5) * -25;
 
-	img.style.transform =
-		'rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg) scale(1.1)'
-	img.classList.add('hover-effect')
+    img.style.transform =
+        'rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg) scale(1.1)';
+    img.classList.add('hover-effect');
+}
+
+// ======== Scroll lock ========
+function disableScroll(e) {
+    e.preventDefault();
+}
+
+function enableScroll() {
+    document.body.style.overflow = '';
 }
 
 if (img) {
-	// ======== MOUSE (PC) ========
-	img.addEventListener('mousemove', function (e) {
-		var rect = img.getBoundingClientRect()
-		var x = e.clientX - rect.left
-		var y = e.clientY - rect.top
+    // ======== MOUSE (PC) ========
+    img.addEventListener('mousemove', function (e) {
+        var rect = img.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
 
-		handleMove(x, y, rect)
-	})
+        handleMove(x, y, rect);
+    });
 
-	// ======== TOUCH (MOBILE) ========
-	img.addEventListener('touchmove', function (e) {
-		var rect = img.getBoundingClientRect()
-		var touch = e.touches[0] // первый палец
+    // ======== TOUCH (MOBILE) ========
+    img.addEventListener('touchstart', function () {
+        document.body.style.overflow = 'hidden'; // блокируем скролл
+    });
 
-		var x = touch.clientX - rect.left
-		var y = touch.clientY - rect.top
+    img.addEventListener('touchmove', function (e) {
+        var rect = img.getBoundingClientRect();
+        var touch = e.touches[0];
 
-		handleMove(x, y, rect)
-	})
+        var x = touch.clientX - rect.left;
+        var y = touch.clientY - rect.top;
 
-	// ======== RESET EFFECT ========
-	img.addEventListener('mouseleave', resetTilt)
-	img.addEventListener('touchend', resetTilt)
+        handleMove(x, y, rect);
 
-	function resetTilt() {
-		img.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)'
-		img.classList.remove('hover-effect')
-	}
+        disableScroll(e); // запрещаем скролл во время движения пальца
+    });
+
+    img.addEventListener('touchend', function () {
+        resetTilt();
+        enableScroll(); // возвращаем скролл
+    });
+
+    // ======== RESET EFFECT ========
+    img.addEventListener('mouseleave', resetTilt);
+
+    function resetTilt() {
+        img.style.transform = 'rotateX(0deg) rotateY(0deg) scale(1)';
+        img.classList.remove('hover-effect');
+    }
 }
